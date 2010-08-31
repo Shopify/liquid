381b4f526801a032b0f87ae488d49d846e12a8a8
Michael Richardson <mcr@sandelman.ca>
2010-08-31 19:42:13 -0400

    adjusted test case to have third argument

--- test/inc_tag_test.rb
+++ test/inc_tag_test.rb
@@ -5,12 +5,12 @@ class IncTagTest < Test::Unit::TestCase
   include Liquid
   
   def test_inc
-    assert_template_result('0','{%inc port %}')  
-    assert_template_result('0 1','{%inc port %} {%inc port%}')  
+    assert_template_result('0','{%inc port %}', {})
+    assert_template_result('0 1','{%inc port %} {%inc port%}', {})
     assert_template_result('0 0 1 2 1',
                            '{%inc port %} {%inc starboard%} ' +
                            '{%inc port %} {%inc port%} ' +
-                           '{%inc starboard %}')
+                           '{%inc starboard %}', {})
   end
   
 end
