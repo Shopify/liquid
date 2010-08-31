ca2fa587cf224e59f46bbeda0de538fba1cf6217
Michael Richardson <mcr@sandelman.ca>
2010-08-31 16:17:12 -0400

    added tag to increment a variable each time it is referenced

---
+++ lib/liquid/tags/inc.rb
@@ -0,0 +1,37 @@
+#require 'rubygems'
+#require 'ruby-debug'
+
+module Liquid
+  
+  # inc is used in a place where one needs to insert a counter
+  #     into a template, and needs the counter to survive across
+  #     multiple instantiations of the template.
+  #
+  #     if the variable does not exist, it is created with value 0.
+
+  #   Hello: {% inc variable %}
+  #
+  # gives you:
+  #
+  #    Hello: 0
+  #    Hello: 1
+  #    Hello: 2
+  #
+  class Inc < Tag
+    def initialize(tag_name, markup, tokens)      
+      @variable = markup.strip
+
+      super    
+    end    
+  
+    def render(context)
+      value = context.registers[@variable] ||= 0
+      context.registers[@variable] = value + 1
+      value.to_s
+    end
+  
+    private
+  end
+  
+  Template.register_tag('inc', Inc)
+end
