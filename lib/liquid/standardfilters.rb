33ecb29d499a25a39c2b49e26844dbeb5972803f
Christopher H. Laco <claco@chrislaco.com>
2010-11-07 20:41:41 -0500

    Update date to accept epoch

--- lib/liquid/standardfilters.rb
+++ lib/liquid/standardfilters.rb
@@ -158,6 +158,10 @@ module Liquid
         return input.to_s
       end
 
+      if ((input.is_a?(String) && !/^\d+$/.match(input.to_s).nil?) || input.is_a?(Integer)) && input.to_i > 0
+        input = Time.at(input.to_i)
+      end
+
       date = input.is_a?(String) ? Time.parse(input) : input
 
       if date.respond_to?(:strftime)
