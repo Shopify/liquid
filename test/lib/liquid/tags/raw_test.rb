4087a94d881c53b803f1d682db7e4676a6f4d714
Jonathan Rudenberg <jonathan@shopify.com>
2011-06-12 11:12:32 -0400

    Add raw tag. Fixes #37.
    Thanks to phaer: https://gist.github.com/1020852
    
    YO DAWG, WE HEARD YOU LIKE LIQUID
    
    SO WE PUT A RAW TAG IN YOUR LIQUID
    SO YOU CAN HAVE LIQUID IN YOUR LIQUID

---
+++ test/lib/liquid/tags/raw_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class RawTest < Test::Unit::TestCase
+  include Liquid
+
+  def test_tag_in_raw
+    assert_template_result '{% comment %} test {% endcomment %}',
+                           '{% raw %}{% comment %} test {% endcomment %}{% endraw %}'
+  end
+
+  def test_output_in_raw
+    assert_template_result '{{ test }}',
+                           '{% raw %}{{ test }}{% endraw %}'
+  end
+end
