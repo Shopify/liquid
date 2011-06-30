4aaf750fa898fefd1df451b75189e9ed2cfa817b
Jonathan Rudenberg <jonathan@shopify.com>
2011-06-30 16:31:37 -0400

    Don't mess with the load path

--- lib/liquid.rb
+++ lib/liquid.rb
@@ -19,8 +19,6 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-
 module Liquid
   FilterSeparator             = /\|/
   ArgumentSeparator           = ','
