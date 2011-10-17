b396f3ead3ae8e026a555dec8ea6345efc557082
Holger Just <hjust@meine-er.de>
2011-10-17 19:53:03 +0200

    Normalize whitespace

--- lib/liquid/tags/include.rb
+++ lib/liquid/tags/include.rb
@@ -1,11 +1,11 @@
 module Liquid
   class Include < Tag
     Syntax = /(#{QuotedFragment}+)(\s+(?:with|for)\s+(#{QuotedFragment}+))?/
-  
-    def initialize(tag_name, markup, tokens)      
+
+    def initialize(tag_name, markup, tokens)
       if markup =~ Syntax
 
-        @template_name = $1        
+        @template_name = $1
         @variable_name = $3
         @attributes    = {}
 
@@ -19,15 +19,15 @@ module Liquid
 
       super
     end
-  
+
     def parse(tokens)
     end
-    
+
     def render(context)
       source = _read_template_from_file_system(context)
       partial = Liquid::Template.parse(source)
       variable = context[@variable_name || @template_name[1..-2]]
-      
+
       context.stack do
         @attributes.each do |key, value|
           context[key] = context[value]
@@ -44,22 +44,22 @@ module Liquid
         end
       end
     end
-   
-    private
-      def _read_template_from_file_system(context)
-        file_system = context.registers[:file_system] || Liquid::Template.file_system
-      
-        # make read_template_file call backwards-compatible.
-        case file_system.method(:read_template_file).arity
-        when 1
-          file_system.read_template_file(context[@template_name])
-        when 2
-          file_system.read_template_file(context[@template_name], context)
-        else
-          raise ArgumentError, "file_system.read_template_file expects two parameters: (template_name, context)"
-        end
+
+  private
+    def _read_template_from_file_system(context)
+      file_system = context.registers[:file_system] || Liquid::Template.file_system
+
+      # make read_template_file call backwards-compatible.
+      case file_system.method(:read_template_file).arity
+      when 1
+        file_system.read_template_file(context[@template_name])
+      when 2
+        file_system.read_template_file(context[@template_name], context)
+      else
+        raise ArgumentError, "file_system.read_template_file expects two parameters: (template_name, context)"
       end
+    end
   end
 
-  Template.register_tag('include', Include)  
+  Template.register_tag('include', Include)
 end
