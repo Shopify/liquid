b48ad7da3ac2d75e2d3ad695badf74b7e0c69c61
Marcus Stollsteimer <sto.mar@web.de>
2012-11-18 10:29:22 +0100

    Remove trailing whitespace

--- lib/liquid/tags/include.rb
+++ lib/liquid/tags/include.rb
@@ -1,11 +1,11 @@
 module Liquid
   class Include < Tag
     Syntax = /(#{QuotedFragment}+)(\s+(?:with|for)\s+(#{QuotedFragment}+))?/o
-  
-    def initialize(tag_name, markup, tokens)      
+
+    def initialize(tag_name, markup, tokens)
       if markup =~ Syntax
 
-        @template_name = $1        
+        @template_name = $1
         @variable_name = $3
         @attributes    = {}
 
@@ -19,15 +19,15 @@ module Liquid
 
       super
     end
-  
+
     def parse(tokens)
     end
-    
+
     def render(context)
       source = _read_template_from_file_system(context)
       partial = Liquid::Template.parse(source)
       variable = context[@variable_name || @template_name[1..-2]]
-      
+
       context.stack do
         @attributes.each do |key, value|
           context[key] = context[value]
@@ -44,11 +44,11 @@ module Liquid
         end
       end
     end
-   
+
     private
       def _read_template_from_file_system(context)
         file_system = context.registers[:file_system] || Liquid::Template.file_system
-      
+
         # make read_template_file call backwards-compatible.
         case file_system.method(:read_template_file).arity
         when 1
@@ -61,5 +61,5 @@ module Liquid
       end
   end
 
-  Template.register_tag('include', Include)  
+  Template.register_tag('include', Include)
 end
