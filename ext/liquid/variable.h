f367dd79155da6f55e512a19f066a0cc0c954f8d
Isha <isha_garg_92@yahoo.com>
2014-02-28 12:35:15 -0500

    don't really need those I suppose

--- ext/liquid/variable.h
+++ ext/liquid/variable.h
@@ -3,18 +3,9 @@
 
 #include <regex.h>
 
-enum error_mode {
-    STRICT,
-    LAX,
-    WARN
-};
-
 struct liquid_variable {
     char *markup; long markup_len;
     char *name; long name_len;
-    enum error_mode e_mode;
-    char **filters;
-    int *filter_len;
 };
 
 void init_liquid_variable();
