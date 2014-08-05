9117722740ca6ed95dac9717ac4ef60cfa976a8d
Florian Weingarten <flo@hackvalue.de>
2014-08-05 14:22:11 +0000

    Use symbols in respond_to?

--- lib/liquid/standardfilters.rb
+++ lib/liquid/standardfilters.rb
@@ -101,7 +101,7 @@ module Liquid
       ary = InputIterator.new(input)
       if property.nil?
         ary.sort
-      elsif ary.first.respond_to?('[]'.freeze) && !ary.first[property].nil?
+      elsif ary.first.respond_to?(:[]) && !ary.first[property].nil?
         ary.sort {|a,b| a[property] <=> b[property] }
       elsif ary.first.respond_to?(property)
         ary.sort {|a,b| a.send(property) <=> b.send(property) }
@@ -114,7 +114,7 @@ module Liquid
       ary = InputIterator.new(input)
       if property.nil?
         input.uniq
-      elsif input.first.respond_to?('[]'.freeze)
+      elsif input.first.respond_to?(:[])
         input.uniq{ |a| a[property] }
       end
     end
