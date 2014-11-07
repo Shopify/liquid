2fe3a21a5d2497d4724dd5ebb3da1708057a4ed2
Florian Weingarten <flo@hackvalue.de>
2014-11-07 01:46:59 +0000

    Revert "Merge pull request #479 from Shopify/tweaks-for-c"
    This reverts commit aa182f64b49fa5d8a902860873802a9db8ee05b8, reversing
    changes made to 70c45f8cd84c753298dd47488b85169458692875.

--- performance/profile.rb
+++ performance/profile.rb
@@ -8,17 +8,10 @@ profiler.run
 [:cpu, :object].each do |profile_type|
   puts "Profiling in #{profile_type.to_s} mode..."
   results = StackProf.run(mode: profile_type) do
-    200.times do
+    100.times do
       profiler.run
     end
   end
-
-  if profile_type == :cpu && graph_filename = ENV['GRAPH_FILENAME']
-    File.open(graph_filename, 'w') do |f|
-      StackProf::Report.new(results).print_graphviz(nil, f)
-    end
-  end
-
   StackProf::Report.new(results).print_text(false, 20)
   File.write(ENV['FILENAME'] + "." + profile_type.to_s, Marshal.dump(results)) if ENV['FILENAME']
 end
