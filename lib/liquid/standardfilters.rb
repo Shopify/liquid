5aea2284cdc37a0c23464474f99285d600443c82
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2016-02-16 09:00:36 -0500

    Add format filter

--- lib/liquid/standardfilters.rb
+++ lib/liquid/standardfilters.rb
@@ -342,6 +342,30 @@ module Liquid
         input
       end
     end
+    
+        
+    # Format a string with Kernel::format
+    #
+    # input - The format string
+    # args - The arguments to input
+    #
+    # Returns the formatted string
+    def format(input, *args)
+      input.taint
+      args.taint
+      
+      args.each_index {|i| args[i] = (Hash[args[i].map {|k,v| [k.to_sym, v]}]) if args[i].instance_of?(Hash) }
+
+      begin
+        return Kernel::format(input, *args)
+      rescue => exception
+        return Kernel::format "%s: %s", exception.class, exception
+      end
+    end
+
+    def sprintf(input, *args)
+      format(input, *args)
+    end
 
     private
 
