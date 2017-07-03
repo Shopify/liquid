b0eaae8da752840d00b8aad09a5b5f0332354331
Rene van Pelt <rene@dunmedia.nl>
2017-07-03 16:38:50 +0200

    added attr_accessors for collection and variable names in for tag

--- lib/liquid/tags/for.rb
+++ lib/liquid/tags/for.rb
@@ -46,6 +46,9 @@ module Liquid
   class For < Block
     Syntax = /\A(#{VariableSegment}+)\s+in\s+(#{QuotedFragment}+)\s*(reversed)?/o
 
+    attr_accessor :collection_name
+    attr_accessor :variable_name
+
     def initialize(tag_name, markup, options)
       super
       @from = @limit = nil
