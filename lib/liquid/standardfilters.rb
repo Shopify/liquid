bfc8979118d272c8c221bcbf7d9135d0c36eb590
Eric Cornelissen <ericornelissen@gmail.com>
2017-07-31 20:11:13 +0200

    Add to_power as standard filter

--- lib/liquid/standardfilters.rb
+++ lib/liquid/standardfilters.rb
@@ -319,6 +319,10 @@ module Liquid
       apply_operation(input, operand, :*)
     end
 
+    def to_power(input, operand)
+      apply_operation(input, operand, :**)
+    end
+
     # division
     def divided_by(input, operand)
       apply_operation(input, operand, :/)
