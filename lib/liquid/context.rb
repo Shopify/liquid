a979b3ec950b6def8dab85bd1c5ecfb4c8b6fd04
Pascal Betz <pascal.betz@gmail.com>
2017-08-18 18:09:57 +0200

    Do not raise when variable is defined but nil when using strict_variables

--- lib/liquid/context.rb
+++ lib/liquid/context.rb
@@ -171,7 +171,9 @@ module Liquid
       if scope.nil?
         @environments.each do |e|
           variable = lookup_and_evaluate(e, key, raise_on_not_found: raise_on_not_found)
-          unless variable.nil?
+          # When lookup returned a value OR there is no value but the lookup also did not raise
+          # then it is the value we are looking for.
+          if !variable.nil? || @strict_variables && raise_on_not_found
             scope = e
             break
           end
