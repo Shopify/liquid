5dcce427d0d868f1ed21130e249517582b48712e
Sam Doiron <samuel.doiron@shopify.com>
2021-01-29 16:48:38 -0500

    Add inline comments syntax

---
+++ lib/liquid/tags/inline_comment.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Liquid
+  class InlineComment < Tag
+    def blank?
+      true
+    end
+
+    def render_to_output_buffer(_context, _output)
+    end
+  end
+
+  Template.register_tag('#', InlineComment)
+end
+
---
+++ test/integration/inline_comment_test.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class InlineCommentTest < Minitest::Test
+  include Liquid
+
+  def test_basic_usage
+    template_source = <<-END_TEMPLATE
+    foo{% # this is a comment %}bar
+    END_TEMPLATE
+    template        = Template.parse(template_source)
+    rendered        = template.render!
+    assert_equal("foobar", rendered.strip)
+  end
+end
+
