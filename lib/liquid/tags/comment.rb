02b6be4025e19cd9d7e6c20f010372ec840aa3a3
Christophe Naud-Dulude <christophe.nauddulude@shopify.com>
2021-11-30 14:10:34 -0500

    Potential fix

--- lib/liquid/tags/comment.rb
+++ lib/liquid/tags/comment.rb
@@ -2,6 +2,19 @@
 
 module Liquid
   class Comment < Block
+    # Potential fix
+    FullTokenPossiblyInvalid = /\A(.*)#{TagStart}#{WhitespaceControl}?\s*(\w+)\s*(.*)?#{WhitespaceControl}?#{TagEnd}\z/om
+
+    def parse(tokens)
+      while (token = tokens.shift)
+        if token =~ FullTokenPossiblyInvalid && block_delimiter == Regexp.last_match(2)
+          return
+        end
+      end
+
+      raise_tag_never_closed(block_name)
+    end
+
     def render_to_output_buffer(_context, output)
       output
     end
