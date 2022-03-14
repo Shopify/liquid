c8af0b06b9503f48f7ad4ecd800f5387c4234b7f
Shaina Raskas <shaina.raskas@shopify.com>
2022-03-14 23:07:42 +0000

    remove spaces

--- lib/liquid/tags/comment.rb
+++ lib/liquid/tags/comment.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 module Liquid
-
   # @public_docs
   # @type tag
   # @category theme
--- lib/liquid/tags/render.rb
+++ lib/liquid/tags/render.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 module Liquid
-
   # @public_docs
   # @type tag
   # @category theme
