f4e32d2214d3c2eae623459b5b775706d1391019
Marco Concetto Rudilosso <marcoconcettorudilosso@gmail.com>
2022-09-08 15:38:18 +0200

    Expose alias_name and for_loop? in render tag

--- lib/liquid/tags/render.rb
+++ lib/liquid/tags/render.rb
@@ -31,7 +31,7 @@ module Liquid
 
     disable_tags "include"
 
-    attr_reader :template_name_expr, :variable_name_expr, :attributes
+    attr_reader :template_name_expr, :variable_name_expr, :attributes, :alias_name, :for_loop?
 
     def initialize(tag_name, markup, options)
       super
@@ -45,7 +45,7 @@ module Liquid
       @alias_name = Regexp.last_match(6)
       @variable_name_expr = variable_name ? parse_expression(variable_name) : nil
       @template_name_expr = parse_expression(template_name)
-      @for = (with_or_for == FOR)
+      @for_loop? = (with_or_for == FOR)
 
       @attributes = {}
       markup.scan(TagAttributes) do |key, value|
@@ -85,7 +85,7 @@ module Liquid
       }
 
       variable = @variable_name_expr ? context.evaluate(@variable_name_expr) : nil
-      if @for && variable.respond_to?(:each) && variable.respond_to?(:count)
+      if @for_loop? && variable.respond_to?(:each) && variable.respond_to?(:count)
         forloop = Liquid::ForloopDrop.new(template_name, variable.count, nil)
         variable.each { |var| render_partial_func.call(var, forloop) }
       else
