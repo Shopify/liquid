2c2f5826d58286e1eb2b31f2a536c2bb5606ee7a
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2022-10-21 11:43:58 -0400

    Change increment/decrement tests to avoid relying on input
    So these can be tested using language tests, without needing support
    for counter value input, which isn't needed if input is provided
    through the static environment.

--- test/integration/tags/increment_tag_test.rb
+++ test/integration/tags/increment_tag_test.rb
@@ -6,20 +6,21 @@ class IncrementTagTest < Minitest::Test
   include Liquid
 
   def test_inc
-    assert_template_result('0', '{%increment port %}', {})
-    assert_template_result('0 1', '{%increment port %} {%increment port%}', {})
+    assert_template_result('0 1', '{%increment port %} {{ port }}')
+    assert_template_result(' 0 1 2', '{{port}} {%increment port %} {%increment port%} {{port}}')
     assert_template_result('0 0 1 2 1',
       '{%increment port %} {%increment starboard%} ' \
       '{%increment port %} {%increment port%} ' \
-      '{%increment starboard %}', {})
+      '{%increment starboard %}')
   end
 
   def test_dec
-    assert_template_result('9', '{%decrement port %}', { 'port' => 10 })
-    assert_template_result('-1 -2', '{%decrement port %} {%decrement port%}', {})
-    assert_template_result('1 5 2 2 5',
+    assert_template_result('-1 -1', '{%decrement port %} {{ port }}', { 'port' => 10 })
+    assert_template_result(' -1 -2 -2', '{{port}} {%decrement port %} {%decrement port%} {{port}}')
+    assert_template_result('0 1 2 0 3 1 1 3',
+      '{%increment starboard %} {%increment starboard%} {%increment starboard%} ' \
       '{%increment port %} {%increment starboard%} ' \
       '{%increment port %} {%decrement port%} ' \
-      '{%decrement starboard %}', { 'port' => 1, 'starboard' => 5 })
+      '{%decrement starboard %}')
   end
 end
