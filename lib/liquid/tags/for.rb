96b5325f873808ae8cbfef471e602d6c5bddd115
Dylan Thacker-Smith <dylan.smith@shopify.com>
2022-10-26 09:21:43 -0400

    Remove Usage.increment calls that have been shown to in fact be used (#1645)
    I think these were added for undocumented features to see if they were
    actually used and they were being used.

--- lib/liquid/tags/for.rb
+++ lib/liquid/tags/for.rb
@@ -177,7 +177,6 @@ module Liquid
       case key
       when 'offset'
         @from = if expr == 'continue'
-          Usage.increment('for_offset_continue')
           :continue
         else
           parse_expression(expr)
