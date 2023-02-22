e650dc419521c402df5ddefdb2d3ac10e0159ac9
Dylan Thacker-Smith <dylan.smith@shopify.com>
2023-02-22 12:48:18 -0500

    Revert "Instrument usage of bug with iteration of String with offset or 0 limit (#1667)" (#1677)
    This reverts commit c743936a78df5e658afa4fb9035bc92ffecd532c.

--- test/unit/tags/for_tag_unit_test.rb
+++ test/unit/tags/for_tag_unit_test.rb
@@ -12,34 +12,4 @@ class ForTagUnitTest < Minitest::Test
     template = Liquid::Template.parse('{% for item in items %}FOR{% else %}ELSE{% endfor %}')
     assert_equal(['FOR', 'ELSE'], template.root.nodelist[0].nodelist.map(&:nodelist).flatten)
   end
-
-  def test_for_string_slice_bug_usage
-    template = Liquid::Template.parse("{% for x in str, offset: 1 %}{{ x }},{% endfor %}")
-    assert_usage("string_slice_bug") do
-      assert_equal("abc,", template.render({ "str" => "abc" }))
-    end
-  end
-
-  def test_for_string_0_limit_usage
-    template = Liquid::Template.parse("{% for x in str, limit: 0 %}{{ x }},{% endfor %}")
-    assert_usage("string_slice_bug") do
-      assert_equal("abc,", template.render({ "str" => "abc" }))
-    end
-  end
-
-  def test_for_string_no_slice_usage
-    template = Liquid::Template.parse("{% for x in str, offset: 0, limit: 1 %}{{ x }},{% endfor %}")
-    assert_usage("string_slice_bug", times: 0) do
-      assert_equal("abc,", template.render({ "str" => "abc" }))
-    end
-  end
-
-  private
-
-  def assert_usage(name, times: 1, &block)
-    count = 0
-    result = Liquid::Usage.stub(:increment, ->(n) { count += 1 if n == name }, &block)
-    assert_equal(times, count)
-    result
-  end
 end
