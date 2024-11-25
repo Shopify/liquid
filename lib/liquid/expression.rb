0fcb2c07eb8f2b0bea40c882660bba489bd8b80a
Michael Go <michael.go@shopify.com>
2024-11-25 15:40:41 -0400

    code styling

--- lib/liquid/expression.rb
+++ lib/liquid/expression.rb
@@ -89,9 +89,7 @@ module Liquid
 
         # Cache only exists during parsing
         if cache
-          return cache[markup] if cache.key?(markup)
-
-          cache[markup] = inner_parse(markup, ss, cache).freeze
+          cache.fetch(markup) { inner_parse(markup, ss, cache).freeze }
         else
           inner_parse(markup, ss, nil).freeze
         end
