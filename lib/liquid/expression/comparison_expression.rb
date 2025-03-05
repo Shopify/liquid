0cacc01376d545bfd5efa7ed24d353b36f503c19
Guilherme Carreiro <guilherme.carreiro@shopify.com>
2025-03-05 14:52:28 +0100

    * Move expression handling from variable.rb to expression.rb * Update test suite to validate parity * Remove parentheses handling * Split boolean into comparison and logical expressions

---
+++ lib/liquid/expression/comparison_expression.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Liquid
+  class Expression
+    class ComparisonExpression
+      COMPARISON_REGEX = /\A\s*(.+?)\s*(==|!=|<>|<=|>=|<|>|contains)\s*(.+)\s*\z/
+
+      class << self
+        def comparison?(markup)
+          markup.match(COMPARISON_REGEX)
+        end
+
+        def parse(markup, ss, cache)
+          match = comparison?(markup)
+
+          if match
+            left = Expression.parse(match[1].strip, ss, cache)
+            operator = match[2].strip
+            right = Expression.parse(match[3].strip, ss, cache)
+
+            return Condition.new(left, operator, right)
+          end
+
+          Condition.new(parse(markup, ss, cache), nil, nil)
+        end
+      end
+    end
+  end
+end
