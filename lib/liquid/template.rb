aefd48e34141ee534edec16d148d7feeb117094d
Justin D. Harris <1594505+juharris@users.noreply.github.com>
2025-04-22 09:50:47 -0400

    template.rb: Correct typo in docs

--- lib/liquid/template.rb
+++ lib/liquid/template.rb
@@ -2,7 +2,7 @@
 
 module Liquid
   # Templates are central to liquid.
-  # Interpretating templates is a two step process. First you compile the
+  # Interpreting templates is a two step process. First you compile the
   # source code you got. During compile time some extensive error checking is performed.
   # your code should expect to get some SyntaxErrors.
   #
