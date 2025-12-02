8efcf7dd3a25bcd5bdc420e775c8a3cc9da7c789
Charles-P. Clermont <charles@cpclermont.com>
2025-12-02 16:19:41 -0500

    Move VariableLookup parsing logic to .parse instead of initializer
    Goal is to get rid of it entirely, but baby steps.

--- test/unit/condition_unit_test.rb
+++ test/unit/condition_unit_test.rb
@@ -82,7 +82,7 @@ class ConditionUnitTest < Minitest::Test
   def test_contains_works_on_arrays
     @context          = Liquid::Context.new
     @context['array'] = [1, 2, 3, 4, 5]
-    array_expr        = VariableLookup.new("array")
+    array_expr        = VariableLookup.parse("array")
 
     assert_evaluates_false(array_expr, 'contains', 0)
     assert_evaluates_true(array_expr, 'contains', 1)
@@ -96,8 +96,8 @@ class ConditionUnitTest < Minitest::Test
 
   def test_contains_returns_false_for_nil_operands
     @context = Liquid::Context.new
-    assert_evaluates_false(VariableLookup.new('not_assigned'), 'contains', '0')
-    assert_evaluates_false(0, 'contains', VariableLookup.new('not_assigned'))
+    assert_evaluates_false(VariableLookup.parse('not_assigned'), 'contains', '0')
+    assert_evaluates_false(0, 'contains', VariableLookup.parse('not_assigned'))
   end
 
   def test_contains_return_false_on_wrong_data_type
@@ -149,7 +149,7 @@ class ConditionUnitTest < Minitest::Test
     @context        = Liquid::Context.new
     @context['one'] = @context['another'] = "gnomeslab-and-or-liquid"
 
-    assert_evaluates_true(VariableLookup.new("one"), '==', VariableLookup.new("another"))
+    assert_evaluates_true(VariableLookup.parse("one"), '==', VariableLookup.parse("another"))
   end
 
   def test_default_context_is_deprecated
