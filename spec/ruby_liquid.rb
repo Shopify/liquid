34c274d3149cedd4fea250cb7e687d7dbb05ef5b
Tobi Lutke <tobi@shopify.com>
2026-01-01 21:42:11 -0500

    Fix rubocop: rename ruby-liquid.rb to ruby_liquid.rb and add trailing comma

--- spec/ruby_liquid.rb
+++ spec/ruby_liquid.rb
@@ -2,7 +2,7 @@
 
 # Liquid Spec Adapter for Shopify/liquid (Ruby reference implementation)
 #
-# Run with: bundle exec liquid-spec run spec/ruby-liquid.rb
+# Run with: bundle exec liquid-spec run spec/ruby_liquid.rb
 
 $LOAD_PATH.unshift(File.expand_path('../lib', __dir__))
 require 'liquid'
@@ -25,7 +25,7 @@ LiquidSpec.render do |template, ctx|
   context = Liquid::Context.build(
     static_environments: ctx.environment,
     registers: registers,
-    rethrow_errors: ctx.rethrow_errors?
+    rethrow_errors: ctx.rethrow_errors?,
   )
 
   context.exception_renderer = ctx.exception_renderer if ctx.exception_renderer
